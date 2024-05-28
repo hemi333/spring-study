17ee0158347e3285c2c6b3956be327387448da6a
Robbie <Robbie@email.com>
2024-05-28 13:34:25 +0900

    init commit

---
+++ src/main/java/com/example/spring_prepare/SpringPrepareApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_prepare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrepareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrepareApplication.class, args);
+	}
+
+}
