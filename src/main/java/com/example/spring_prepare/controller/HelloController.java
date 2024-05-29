8381c50af2f78953e29b674d3d5327e108452f19
Robbie <hemi33379@gmail.com>
2024-05-29 17:11:37 +0900

    postman test2

--- src/main/java/com/example/spring_prepare/controller/HelloController.java
+++ src/main/java/com/example/spring_prepare/controller/HelloController.java
@@ -1,13 +1,40 @@
 package com.example.spring_prepare.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
+@RequestMapping("/api")
 public class HelloController {
-    @GetMapping("/api/hello")
+
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/get")
+    @ResponseBody
+    public String get() {
+        return "GET Method 요청";
+    }
+
+    @PostMapping("/post")
+    @ResponseBody
+    public String post() {
+        return "POST Method 요청";
+    }
+
+    @PutMapping("/put")
+    @ResponseBody
+    public String put() {
+        return "PUT Method 요청";
+    }
+
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public String delete() {
+        return "DELETE Method 요청";
     }
 
 }
