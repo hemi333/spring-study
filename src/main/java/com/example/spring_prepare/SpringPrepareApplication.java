6af6beaf50e3be9466b307170b098c37757cf585
Robbie <hemi33379@gmail.com>
2024-05-29 16:15:26 +0900

    Failed to configure a DataSource 에러 임시 수정

--- src/main/java/com/example/spring_prepare/SpringPrepareApplication.java
+++ src/main/java/com/example/spring_prepare/SpringPrepareApplication.java
@@ -2,12 +2,13 @@ package com.example.spring_prepare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SpringPrepareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPrepareApplication.class, args);
 	}
-
 }
+
