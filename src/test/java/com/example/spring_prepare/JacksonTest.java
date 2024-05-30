4376327933b7d8b96a5bbf8da57b219252a5e40e
Robbie <hemi33379@gmail.com>
2024-05-30 16:26:04 +0900

    jackson test

---
+++ src/test/java/com/example/spring_prepare/JacksonTest.java
@@ -0,0 +1,33 @@
+package com.example.spring_prepare;
+
+import com.example.spring_prepare.response.Star;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JacksonTest {
+
+    @Test
+    @DisplayName("Object To JSON : get Method 필요")
+    void test1() throws JsonProcessingException {
+        Star star = new Star("Robbie", 95);
+
+        ObjectMapper objectMapper = new ObjectMapper(); // Jackson 라이브러리의 ObjectMapper
+        String json = objectMapper.writeValueAsString(star);
+
+        System.out.println("json = " + json);
+    }
+
+    @Test
+    @DisplayName("JSON To Object : 기본 생성자 & (get OR set) Method 필요")
+    void test2() throws JsonProcessingException {
+        String json = "{\"name\":\"Robbie\",\"age\":95}"; // JSON 타입의 String
+
+        ObjectMapper objectMapper = new ObjectMapper(); // Jackson 라이브러리의 ObjectMapper
+
+        Star star = objectMapper.readValue(json, Star.class);
+        System.out.println("star.getName() = " + star.getName());
+        System.out.println("star.getAge() = " + star.getAge());
+    }
+}
