4faa79cd4d1bc7a08d536b02664c1a7599a5d52f
Robbie <hemi33379@gmail.com>
2024-05-30 14:39:28 +0900

    Client에 데이터 반환

---
+++ src/main/java/com/example/spring_prepare/response/ResponseController.java
@@ -0,0 +1,31 @@
+package com.example.spring_prepare.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// RestController 미사용
+@Controller
+@RequestMapping("/response")
+public class ResponseController {
+    // [Response header]
+    //  Content-Type: text/html
+    // [Response body]
+    //  {"name":"Robbie","age":95}
+    @GetMapping("/json/string")
+    @ResponseBody
+    public String helloStringJson() {
+        return "{\"name\":\"Robbie\",\"age\":95}";
+    }
+
+    // [Response header]
+    //  Content-Type: application/json
+    // [Response body]
+    //  {"name":"Robbie","age":95}
+    @GetMapping("/json/class")
+    @ResponseBody
+    public Star helloClassJson() {
+        return new Star("Robbie", 95);
+    }
+}
---
+++ src/main/java/com/example/spring_prepare/response/Star.java
@@ -0,0 +1,16 @@
+package com.example.spring_prepare.response;
+
+import lombok.Getter;
+
+@Getter
+public class Star {
+    String name;
+    int age;
+
+    public Star(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Star() {}
+}
