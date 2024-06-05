b808f6ab3225a29771cd9b06b84fa5dd98469141
Robbie <hemi33379@gmail.com>
2024-06-05 16:23:28 +0900

    java 환경에서의 JPA / debugging

---
+++ src/main/java/com/example/spring_prepare/entity/MemoTest.java
@@ -0,0 +1,46 @@
+package com.example.spring_prepare.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // JPA가 관리할 수 있는 Entity 클래스 지정
+@Table(name = "memo") // 매핑할 테이블의 이름을 지정
+public class MemoTest {
+    @Id
+    private Long id;
+
+    // nullable: null 허용 여부
+    // unique: 중복 허용 여부 (false 일때 중복 허용)
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    // length: 컬럼 길이 지정
+    @Column(name = "contents", nullable = false, length = 500)
+    private String contents;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
