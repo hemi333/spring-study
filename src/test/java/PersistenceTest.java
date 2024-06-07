a1de1142ab88f6cff1c88f8a0af4e888c3b1bd07
Robbie <hemi33379@gmail.com>
2024-06-07 11:08:29 +0900

    Entity 저장 Test

---
+++ src/test/java/PersistenceTest.java
@@ -0,0 +1,47 @@
+import com.example.spring_prepare.entity.Memo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PersistenceTest {
+    EntityManagerFactory emf;
+    EntityManager em;
+
+    @BeforeEach
+    void setUp() {
+        emf = Persistence.createEntityManagerFactory("memo");
+        em = emf.createEntityManager();
+    }
+
+    @Test
+    @DisplayName("1차 캐시 : Entity 저장")
+    void test1() {
+        EntityTransaction et = em.getTransaction();
+
+        et.begin();
+
+        try {
+
+            Memo memo = new Memo();
+            memo.setId(1L);
+            memo.setUsername("Robbie");
+            memo.setContents("1차 캐시 Entity 저장");
+
+            em.persist(memo);
+
+            et.commit();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            et.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
