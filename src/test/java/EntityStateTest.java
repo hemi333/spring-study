20a1748bb245868aa522bae0b4092d327f5ba764
Robbie <hemi33379@gmail.com>
2024-06-10 15:13:02 +0900

    준영속 상태 : detach()
    영속성 컨텍스트로 관리되고 있는 영속 상태(Managed) 일 때만 변경 감지(Dirty Checking)가 진행되고, detach 호출해서 분리된 상태는 더 이상 관리받지 못하기 때문에 변경 감지가 이루어 지지 않음

--- src/test/java/EntityStateTest.java
+++ src/test/java/EntityStateTest.java
@@ -44,4 +44,43 @@ public class EntityStateTest {
 
         emf.close();
     }
+
+    @Test
+    @DisplayName("준영속 상태 : detach()")
+    void test2() {
+        EntityTransaction et = em.getTransaction();
+
+        et.begin();
+
+        try {
+
+            Memo memo = em.find(Memo.class, 1);
+            System.out.println("memo.getId() = " + memo.getId());
+            System.out.println("memo.getUsername() = " + memo.getUsername());
+            System.out.println("memo.getContents() = " + memo.getContents());
+
+            // em.contains(entity) : Entity 객체가 현재 영속성 컨텍스트에 저장되어 관리되는 상태인지 확인하는 메서드
+            System.out.println("em.contains(memo) = " + em.contains(memo));
+
+            System.out.println("detach() 호출");
+            em.detach(memo);
+            System.out.println("em.contains(memo) = " + em.contains(memo));
+
+            System.out.println("memo Entity 객체 수정 시도");
+            memo.setUsername("Update");
+            memo.setContents("memo Entity Update");
+
+            System.out.println("트랜잭션 commit 전");
+            et.commit();
+            System.out.println("트랜잭션 commit 후");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            et.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
 }
