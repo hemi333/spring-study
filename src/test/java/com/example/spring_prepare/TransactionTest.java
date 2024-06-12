c4ef2746dc42b1ca0ac0c3cb0f0eb609a3f9671b
Robbie <hemi33379@gmail.com>
2024-06-12 11:56:21 +0900

    트랜잭션 테스트 : 메모 생성 실패
    트랜잭션 환경이 적용되어 있지 않음

--- src/test/java/com/example/spring_prepare/TransactionTest.java
+++ src/test/java/com/example/spring_prepare/TransactionTest.java
@@ -3,6 +3,7 @@ package com.example.spring_prepare;
 import com.example.spring_prepare.entity.Memo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +22,17 @@ public class TransactionTest {
     void test1() {
         Memo memo = new Memo();
         memo.setUsername("Robbert");
+        memo.setContents("@Transactional 테스트 중!");
+
+        em.persist(memo);  // 영속성 컨텍스트에 메모 Entity 객체를 저장합니다.
+    }
+
+    @Test
+    @Disabled // 해당 method를 테스트 하지 않음
+    @DisplayName("메모 생성 실패")
+    void test2() {
+        Memo memo = new Memo();
+        memo.setUsername("Robbie");
         memo.setContents("@Transactional 테스트 중!");
 
         em.persist(memo);  // 영속성 컨텍스트에 메모 Entity 객체를 저장합니다.
