63d590e71657c13d0809fc78a98ee9d852df18b6
Robbie <hemi33379@gmail.com>
2024-06-12 13:16:57 +0900

    트랜잭션 전파 테스트

--- src/test/java/com/example/spring_prepare/TransactionTest.java
+++ src/test/java/com/example/spring_prepare/TransactionTest.java
@@ -1,19 +1,25 @@
 package com.example.spring_prepare;
 
 import com.example.spring_prepare.entity.Memo;
+import com.example.spring_prepare.repository.MemoRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 public class TransactionTest {
+
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemoRepository memoRepository;
 
     @Test
     @Transactional
@@ -36,5 +42,15 @@ public class TransactionTest {
         memo.setContents("@Transactional 테스트 중!");
 
         em.persist(memo);  // 영속성 컨텍스트에 메모 Entity 객체를 저장합니다.
+    }
+
+    @Test
+    @Disabled
+    //    @Transactional
+    //    @Rollback(value = false)
+    @DisplayName("트랜잭션 전파 테스트")
+    void test3() {
+        //        memoRepository.createMemo(em);
+        System.out.println("테스트 test3 메서드 종료");
     }
 }
