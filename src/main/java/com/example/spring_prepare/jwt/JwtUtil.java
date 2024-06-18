d075b44987c3bb11cac2d4cd34e97c910c6fb61d
hyemi <hemi33379@gmail.com>
2024-06-18 10:51:14 +0900

    토큰 검증

--- src/main/java/com/example/spring_prepare/jwt/JwtUtil.java
+++ src/main/java/com/example/spring_prepare/jwt/JwtUtil.java
@@ -1,8 +1,7 @@
 package com.example.spring_prepare.jwt;
 
 import com.example.spring_prepare.entity.UserRoleEnum;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.Cookie;
@@ -82,6 +81,23 @@ public class JwtUtil {
         }
         logger.error("Not Found Token");
         throw new NullPointerException("Not Found Token");
+    }
+
+    // 토큰 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException | SignatureException e) {
+            logger.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+        } catch (ExpiredJwtException e) {
+            logger.error("Expired JWT token, 만료된 JWT token 입니다.");
+        } catch (UnsupportedJwtException e) {
+            logger.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+        }
+        return false;
     }
 
 }
