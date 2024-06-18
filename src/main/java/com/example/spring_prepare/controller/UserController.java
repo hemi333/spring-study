2a86968a83b5beea2bdff95f66ab5df96d4e882a
hyemi <hemi33379@gmail.com>
2024-06-18 14:40:38 +0900

    회원가입 구현 - controller 생성

---
+++ src/main/java/com/example/spring_prepare/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.spring_prepare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+}
