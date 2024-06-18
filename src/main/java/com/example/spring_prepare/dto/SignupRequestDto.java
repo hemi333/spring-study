5a85b17ff3e7d3033276fb8cc9d1ae213c77711b
hyemi <hemi33379@gmail.com>
2024-06-18 15:37:32 +0900

    create Dto

---
+++ src/main/java/com/example/spring_prepare/dto/SignupRequestDto.java
@@ -0,0 +1,14 @@
+package com.example.spring_prepare.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupRequestDto {
+    private String username;
+    private String password;
+    private String email;
+    private boolean admin = false;
+    private String adminToken = "";
+}
