0237cf97869fe41c9637bf84b63807c72091f7fe
hyemi <hemi33379@gmail.com>
2024-06-18 15:48:10 +0900

    update Controller

--- src/main/java/com/example/spring_prepare/controller/UserController.java
+++ src/main/java/com/example/spring_prepare/controller/UserController.java
@@ -1,12 +1,21 @@
 package com.example.spring_prepare.controller;
 
+import com.example.spring_prepare.dto.SignupRequestDto;
+import com.example.spring_prepare.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/user/login-page")
     public String loginPage() {
@@ -16,5 +25,12 @@ public class UserController {
     @GetMapping("/user/signup")
     public String signupPage() {
         return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(SignupRequestDto requestDto) {
+        userService.signup(requestDto);
+
+        return "redirect:/api/user/login-page";
     }
 }
