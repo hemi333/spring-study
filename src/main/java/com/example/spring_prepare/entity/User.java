6b288fc9c5875587816b6b0f9e1a8f56c616c0c5
hyemi <hemi33379@gmail.com>
2024-06-18 15:19:57 +0900

    add User entity

---
+++ src/main/java/com/example/spring_prepare/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.spring_prepare.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+}
