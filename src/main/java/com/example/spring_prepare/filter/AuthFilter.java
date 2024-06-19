6499ea1a10314d52b9dc1ec1bd0764896eb1ac95
hyemi <hemi33379@gmail.com>
2024-06-19 15:12:45 +0900

    검증 확인 controller 추가

--- src/main/java/com/example/spring_prepare/filter/AuthFilter.java
+++ src/main/java/com/example/spring_prepare/filter/AuthFilter.java
@@ -34,6 +34,7 @@ public class AuthFilter implements Filter {
         if (StringUtils.hasText(url) &&
                 (url.startsWith("/api/user") || url.startsWith("/css") || url.startsWith("/js"))
         ) {
+            log.info("인증 처리를 하지 않는 URL : " + url);
             // 회원가입, 로그인 관련 API 는 인증 필요없이 요청 진행
             chain.doFilter(request, response); // 다음 Filter 로 이동
         } else {
