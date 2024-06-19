6b1681cc17e4587d7b375491df51b894d6adf046
hyemi <hemi33379@gmail.com>
2024-06-19 14:27:00 +0900

    Logging Filter

---
+++ src/main/java/com/example/spring_prepare/filter/LoggingFilter.java
@@ -0,0 +1,27 @@
+package com.example.spring_prepare.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j(topic = "LoggingFilter")
+@Component
+@Order(1)
+public class LoggingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 전처리
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String url = httpServletRequest.getRequestURI();
+        log.info(url);
+
+        chain.doFilter(request, response); // 다음 Filter 로 이동
+
+        // 후처리
+        log.info("비즈니스 로직 완료");
+    }
+}
