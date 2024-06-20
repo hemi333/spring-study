44a2764654a1a12e26cf5ef39008bcbdae9030de
hyemi <hemi33379@gmail.com>
2024-06-20 16:55:17 +0900

    Spring Security 로그인
    Spring Security 기본 제공 로그인이 아니라 custom 해서 사용

--- src/main/java/com/example/spring_prepare/config/WebSecurityConfig.java
+++ src/main/java/com/example/spring_prepare/config/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.spring_prepare.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,12 +19,23 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-//                        .requestMatchers("/api/user/**").permitAll()
+                        .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
         // 로그인 사용
-        http.formLogin(Customizer.withDefaults());
+        http.formLogin((formLogin) ->
+                formLogin
+                        // 로그인 View 제공 (GET /api/user/login-page)
+                        .loginPage("/api/user/login-page")
+                        // 로그인 처리 (POST /api/user/login)
+                        .loginProcessingUrl("/api/user/login")
+                        // 로그인 처리 후 성공 시 URL
+                        .defaultSuccessUrl("/")
+                        // 로그인 처리 후 실패 시 URL
+                        .failureUrl("/api/user/login-page?error")
+                        .permitAll()
+        );
 
         return http.build();
     }
