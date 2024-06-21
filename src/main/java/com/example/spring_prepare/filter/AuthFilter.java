14f5854bfa1fe50596e4e3aca639217abd96deb2
hyemi <hemi33379@gmail.com>
2024-06-21 10:36:56 +0900

    Filter에서 login 처리를 하기 위한 분리 작업

--- src/main/java/com/example/spring_prepare/filter/AuthFilter.java
+++ src/main/java/com/example/spring_prepare/filter/AuthFilter.java
@@ -8,7 +8,6 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
--- src/main/java/com/example/spring_prepare/service/UserService.java
+++ src/main/java/com/example/spring_prepare/service/UserService.java
@@ -1,13 +1,10 @@
 package com.example.spring_prepare.service;
 
-import com.example.spring_prepare.dto.LoginRequestDto;
 import com.example.spring_prepare.dto.SignupRequestDto;
 import com.example.spring_prepare.entity.User;
 import com.example.spring_prepare.entity.UserRoleEnum;
 import com.example.spring_prepare.jwt.JwtUtil;
 import com.example.spring_prepare.repository.UserRepository;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -59,25 +56,5 @@ public class UserService {
         // 사용자 등록
         User user = new User(username, password, email, role);
         userRepository.save(user);
-    }
-
-    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
-        String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
-
-        // 사용자 확인
-        User user = userRepository.findByUsername(username).orElseThrow(
-                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
-        );
-
-        // 비밀번호 확인
-        if(!passwordEncoder.matches(password, user.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
-
-        // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
-        String token = jwtUtil.createToken(user.getUsername(), user.getRole());
-        jwtUtil.addJwtToCookie(token, res);
-
     }
 }
