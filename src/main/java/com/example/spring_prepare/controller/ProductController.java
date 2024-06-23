70e07995d2901a4df23ed456130e3d53ecacb6b6
hyemi <hemi33379@gmail.com>
2024-06-23 23:26:15 +0900

    Validation

--- src/main/java/com/example/spring_prepare/controller/ProductController.java
+++ src/main/java/com/example/spring_prepare/controller/ProductController.java
@@ -1,14 +1,15 @@
 package com.example.spring_prepare.controller;
 
+import com.example.spring_prepare.dto.ProductRequestDto;
 import com.example.spring_prepare.entity.User;
 import com.example.spring_prepare.entity.UserRoleEnum;
 import com.example.spring_prepare.security.UserDetailsImpl;
+import jakarta.validation.Valid;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api")
@@ -32,5 +33,11 @@ public class ProductController {
         }
 
         return "redirect:/";
+    }
+
+    @PostMapping("/validation")
+    @ResponseBody
+    public ProductRequestDto testValid(@RequestBody @Valid ProductRequestDto requestDto) {
+        return requestDto;
     }
 }
