84b0596dc0e3bff17c4702906f2afc2b66445a2a
hyemi <hemi33379@gmail.com>
2024-06-26 11:37:50 +0900

    init commit

--- src/main/java/com/example/spring_resttemplate_client/SpringResttemplateClientApplication.java
+++ src/main/java/com/example/spring_resttemplate_client/SpringResttemplateClientApplication.java
@@ -1,13 +1,13 @@
-package com.example.spring_prepare;
+package com.example.spring_resttemplate_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
-public class SpringPrepareApplication {
+public class SpringResttemplateClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringPrepareApplication.class, args);
+		SpringApplication.run(SpringResttemplateClientApplication.class, args);
 	}
+
 }
