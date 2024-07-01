b10c9f6a9352aa4aafc4203ed3406fb66767a3b5
hyemi <hemi33379@gmail.com>
2024-07-01 13:21:00 +0900

    RestTemplate Get 요청_client

--- src/main/java/com/example/spring_resttemplate_client/dto/ItemDto.java
+++ src/main/java/com/example/spring_resttemplate_client/dto/ItemDto.java
@@ -2,10 +2,16 @@ package com.example.spring_resttemplate_client.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.json.JSONObject;
 
 @Getter
 @NoArgsConstructor
 public class ItemDto {
     private String title;
     private int price;
+
+    public ItemDto(JSONObject itemJson) {
+        this.title = itemJson.getString("title");
+        this.price = itemJson.getInt("price");
+    }
 }
--- src/main/java/com/example/spring_resttemplate_client/service/RestTemplateService.java
+++ src/main/java/com/example/spring_resttemplate_client/service/RestTemplateService.java
@@ -2,20 +2,63 @@ package com.example.spring_resttemplate_client.service;
 
 import com.example.spring_resttemplate_client.dto.ItemDto;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Service
 public class RestTemplateService {
 
+    private final RestTemplate restTemplate;
+
+    // RestTemplateBuilder의 build()를 사용하여 RestTemplate을 생성합니다.
+    public RestTemplateService(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
+
     public ItemDto getCallObject(String query) {
-        return null;
+        // 요청 URL 만들기
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:7070")
+                .path("/api/server/get-call-obj")
+                .queryParam("query", query)
+                .encode()
+                .build()
+                .toUri();
+        log.info("uri = " + uri);
+
+        ResponseEntity<ItemDto> responseEntity = restTemplate.getForEntity(uri, ItemDto.class);
+
+        log.info("statusCode = " + responseEntity.getStatusCode());
+
+        return responseEntity.getBody();
     }
 
     public List<ItemDto> getCallList() {
-        return null;
+        // 요청 URL 만들기
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:7070")
+                .path("/api/server/get-call-list")
+                .encode()
+                .build()
+                .toUri();
+        log.info("uri = " + uri);
+
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+
+        log.info("statusCode = " + responseEntity.getStatusCode());
+        log.info("Body = " + responseEntity.getBody());
+
+        return fromJSONtoItems(responseEntity.getBody());
     }
 
     public ItemDto postCall(String query) {
@@ -24,5 +67,18 @@ public class RestTemplateService {
 
     public List<ItemDto> exchangeCall(String token) {
         return null;
+    }
+
+    public List<ItemDto> fromJSONtoItems(String responseEntity) {
+        JSONObject jsonObject = new JSONObject(responseEntity);
+        JSONArray items  = jsonObject.getJSONArray("items");
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (Object item : items) {
+            ItemDto itemDto = new ItemDto((JSONObject) item);
+            itemDtoList.add(itemDto);
+        }
+
+        return itemDtoList;
     }
 }
