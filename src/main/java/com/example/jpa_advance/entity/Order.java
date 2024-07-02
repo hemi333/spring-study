25db87d6508f5783aab7a897362c7023458a4330
hyemi <hemi33379@gmail.com>
2024-07-02 16:10:23 +0900

    중간 테이블 Order 주문일 추가

--- src/main/java/com/example/jpa_advance/entity/Order.java
+++ src/main/java/com/example/jpa_advance/entity/Order.java
@@ -3,11 +3,16 @@ package com.example.jpa_advance.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "orders")
+@EntityListeners(AuditingEntityListener.class)
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +25,8 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime orderDate;
 }
