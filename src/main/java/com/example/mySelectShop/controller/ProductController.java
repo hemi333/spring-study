20a020d11b19bcfa30fe25f3d96796c099d03c1a
hyemi <hemi33379@gmail.com>
2024-07-03 16:38:40 +0900

    관심 상품 희망 최저가 업데이트 API

--- src/main/java/com/example/mySelectShop/controller/ProductController.java
+++ src/main/java/com/example/mySelectShop/controller/ProductController.java
@@ -1,14 +1,12 @@
 package com.example.mySelectShop.controller;
 
+import com.example.mySelectShop.dto.ProductMypriceRequestDto;
 import com.example.mySelectShop.dto.ProductRequestDto;
 import com.example.mySelectShop.dto.ProductResponseDto;
 import com.example.mySelectShop.entity.Product;
 import com.example.mySelectShop.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +18,10 @@ public class ProductController {
     @PostMapping("/products")
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
         return productService.createProduct(requestDto);
+    }
+
+    @PutMapping("/products/{id}")
+    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+        return productService.updateProduct(id, requestDto);
     }
 }
--- src/main/java/com/example/mySelectShop/service/ProductService.java
+++ src/main/java/com/example/mySelectShop/service/ProductService.java
@@ -1,11 +1,13 @@
 package com.example.mySelectShop.service;
 
+import com.example.mySelectShop.dto.ProductMypriceRequestDto;
 import com.example.mySelectShop.dto.ProductRequestDto;
 import com.example.mySelectShop.dto.ProductResponseDto;
 import com.example.mySelectShop.entity.Product;
 import com.example.mySelectShop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +15,27 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    public static final int MIN_MY_PRICE = 100;
+
     public ProductResponseDto createProduct(ProductRequestDto requestDto) {
         Product product = productRepository.save(new Product(requestDto));
         return new ProductResponseDto(product);
+    }
+
+    @Transactional // Dirty Checking 해야함
+    public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        int myprice = requestDto.getMyprice();
+        if (myprice < MIN_MY_PRICE) {
+            throw new IllegalArgumentException("유효하지 않은 가격입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해 주세요.");
+        }
+
+        Product product = productRepository.findById(id).orElseThrow(() ->
+                new NullPointerException("해당 상품을 찾을 수 없습니다.")
+        );
+
+        product.update(requestDto);
+
+        return new ProductResponseDto(product);
+
     }
 }
