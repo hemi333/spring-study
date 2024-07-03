708bc2a7b884c729ec76de631fb402c27ce1e153
hyemi <hemi33379@gmail.com>
2024-07-03 15:43:29 +0900

    상품 검색 API 구현

---
+++ src/main/java/com/example/mySelectShop/naver/controller/NaverApiController.java
@@ -0,0 +1,24 @@
+package com.example.mySelectShop.naver.controller;
+
+import com.example.mySelectShop.naver.dto.ItemDto;
+import com.example.mySelectShop.naver.service.NaverApiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class NaverApiController {
+
+    private final NaverApiService naverApiService;
+
+    @GetMapping("/search")
+    public List<ItemDto> searchItems(@RequestParam String query)  {
+        return naverApiService.searchItems(query);
+    }
+}
