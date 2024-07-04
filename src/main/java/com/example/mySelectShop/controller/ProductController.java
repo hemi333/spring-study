6f596831157e21037e5ee3b1fc21f893996b5653
hyemi <hemi33379@gmail.com>
2024-07-04 11:15:33 +0900

    상품 페이징 및 정렬

--- src/main/java/com/example/mySelectShop/controller/ProductController.java
+++ src/main/java/com/example/mySelectShop/controller/ProductController.java
@@ -7,6 +7,7 @@ import com.example.mySelectShop.entity.Product;
 import com.example.mySelectShop.security.UserDetailsImpl;
 import com.example.mySelectShop.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,12 +31,12 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<ProductResponseDto> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-       return productService.getProducts(userDetails.getUser());
-    }
-
-    @GetMapping("/admin/products")
-    public List<ProductResponseDto> getAllProducts() {
-        return productService.getAllProducts();
+    public Page<ProductResponseDto> getProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+       return productService.getProducts(userDetails.getUser(),page-1, size, sortBy, isAsc);
     }
 }
--- src/main/java/com/example/mySelectShop/repository/ProductRepository.java
+++ src/main/java/com/example/mySelectShop/repository/ProductRepository.java
@@ -2,10 +2,12 @@ package com.example.mySelectShop.repository;
 
 import com.example.mySelectShop.entity.Product;
 import com.example.mySelectShop.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findAllByUser(User user);
+    Page<Product> findAllByUser(User user, Pageable pageable);
 }
