75e1240570d1477ddb5f12f72ef2ead17a333e9c
hyemi <hemi33379@gmail.com>
2024-07-04 11:37:42 +0900

    상품 폴더 설계

---
+++ src/main/java/com/example/mySelectShop/repository/ProductFolderRepository.java
@@ -0,0 +1,7 @@
+package com.example.mySelectShop.repository;
+
+import com.example.mySelectShop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+}
